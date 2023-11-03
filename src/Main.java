3b0c122219550eb38b2b3a6fc07e75076e761359
BT_2N2_11 <sample@bths.edu>
2023-11-03 14:01:55 -0400

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,99 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StringLoops loops = new StringLoops();
+        System.out.println(loops.removeA("are apples tasty and find the area"));
+        System.out.println(loops.replaceCharacterV1("a", "canned beans and bananas", "*"));
+        System.out.println(loops.replaceCharacterV1(" ", "canned beans and bananas", ""));
+        System.out.println(loops.replaceCharacterV1("t", "Ratatattat", "p"));
+        System.out.println(loops.replaceCharacterV1("!", "Nice! Day today!", "?"));
+        System.out.println(loops.replaceCharacterV1("i", "it’s the brain drain pain train", "I"));
+        System.out.println(loops.replaceCharacterV1("i", "it’s the brain drain pain train", " "));
+        System.out.println(loops.replaceCharacterV1("A", "ABABABABABC", "D"));
+        System.out.println(loops.replaceCharacterV1("a", "ABABABABABC", "D"));
+
+        System.out.println();
+        System.out.println(loops.replaceCharacterV2("a", "canned beans and bananas", "*"));
+        System.out.println(loops.replaceCharacterV2(" ", "canned beans and bananas", ""));
+        System.out.println(loops.replaceCharacterV2("t", "Ratatattat", "p"));
+        System.out.println(loops.replaceCharacterV2("!", "Nice! Day today!", "?"));
+        System.out.println(loops.replaceCharacterV2("i", "it’s the brain drain pain train", "I"));
+        System.out.println(loops.replaceCharacterV2("i", "it’s the brain drain pain train", " "));
+        System.out.println(loops.replaceCharacterV2("A", "ABABABABABC", "D"));
+        System.out.println(loops.replaceCharacterV2("a", "ABABABABABC", "D"));
+
+        System.out.println();
+        System.out.println(loops.countString("an", "canned beans and bananas"));
+        System.out.println(loops.countString("tat", "Ratatattat"));
+        System.out.println(loops.countString("lower", "sunflower"));
+        System.out.println(loops.countString("haha", "Hahahahaha"));
+        System.out.println(loops.countString("rain", "rain drain pain train"));
+        System.out.println(loops.countString("was", "I was about to call you"));
+        System.out.println(loops.countString("but", "I was about to call you"));
+        System.out.println(loops.countString("i", "iiiit's supercalifragilisticexpialidociousi"));
+
+        System.out.println();
+        System.out.println(loops.removeString("an", "canned beans and bananas"));
+        System.out.println(loops.removeString(" ", "canned beans and bananas"));
+        System.out.println(loops.removeString("tat", "Ratatattat"));
+        System.out.println(loops.removeString("lower", "sunflower"));
+        System.out.println(loops.removeString("haha", "hahahahaha"));
+        System.out.println(loops.removeString("ha", "hahahahaha"));
+        System.out.println(loops.removeString("hah", "hahahahaha"));
+        System.out.println(loops.removeString("ah", "aaaaaahhhhhh!")); // careful!
+        System.out.println(loops.removeString("rain", "it’s the brain drain pain train"));
+        System.out.println(loops.removeString("lol", "lolololol"));
+        System.out.println(loops.removeString("was", "I was about to call you"));
+        System.out.println(loops.removeString("but", "I was about to call you"));
+        System.out.println(loops.removeString("AB", "ABABABABABC"));
+        System.out.println(loops.removeString("AB", "AAAAABBBBBC"));  // careful!
+        System.out.println(loops.removeString("i", "iiiit's supercalifragilisticexpialidociousi"));
+
+        System.out.println();
+        loops.commaSeparated(0, 5);
+        loops.commaSeparated(-3, 2);
+        loops.commaSeparated(8, 1);
+        loops.commaSeparated(-4, -10);
+        loops.commaSeparated(4, 4);
+        loops.commaSeparated(0, 0);
+        loops.commaSeparated(0, 1);
+        loops.commaSeparated(-3, 12);
+
+        System.out.println();
+        System.out.println(loops.isPalindrome("A"));
+        System.out.println(loops.isPalindrome("mom"));
+        System.out.println(loops.isPalindrome("dad"));
+        System.out.println(loops.isPalindrome("bob"));
+        System.out.println(loops.isPalindrome("noon"));
+        System.out.println(loops.isPalindrome("deed"));
+        System.out.println(loops.isPalindrome("12321"));
+        System.out.println(loops.isPalindrome("aaaaaaa"));
+        System.out.println(loops.isPalindrome("baaabbaaab"));
+        System.out.println(loops.isPalindrome("civic"));
+        System.out.println(loops.isPalindrome("kayak"));
+        System.out.println(loops.isPalindrome("racecar"));
+        System.out.println(loops.isPalindrome("my gym"));
+        System.out.println(loops.isPalindrome("step on no pets"));
+        System.out.println(loops.isPalindrome("45TTTT54"));
+        System.out.println(loops.isPalindrome("no lemon no melon"));
+        System.out.println(loops.isPalindrome("a nut for a jar of tuna"));
+        System.out.println(loops.isPalindrome("! 1 2 3 4 M 6M4321!"));
+        System.out.println(loops.isPalindrome("was it a car or a cat i saw"));
+        System.out.println(loops.isPalindrome("a man a plan a canal panama"));
+        System.out.println("-------");
+        System.out.println(loops.isPalindrome("mod"));
+        System.out.println(loops.isPalindrome("dang"));
+        System.out.println(loops.isPalindrome("boo"));
+        System.out.println(loops.isPalindrome("noonish"));
+        System.out.println(loops.isPalindrome("indeed"));
+        System.out.println(loops.isPalindrome("1234432"));
+        System.out.println(loops.isPalindrome("civics"));
+        System.out.println(loops.isPalindrome("kayaks"));
+        System.out.println(loops.isPalindrome("racing car"));
+        System.out.println(loops.isPalindrome("my gyms"));
+        System.out.println(loops.isPalindrome("step on pets"));
+        System.out.println(loops.isPalindrome("1 3 2 4 3 2 1"));
+        System.out.println(loops.isPalindrome("AAADAA"));
+        System.out.println(loops.isPalindrome("BaaBaaaB"));
+
     }
 }
--- src/StringLoops.java
+++ src/StringLoops.java
@@ -1,2 +1,135 @@
-package PACKAGE_NAME;public class StringLoops {
+public class StringLoops {
+    // default constructor; no instance variables
+    public StringLoops() { }
+
+
+    /* Returns the number of times "character" appears in "searchString"
+    This should be NON-case sensitive!
+
+
+    Examples:
+    - if character = "a" and searchString = "Apple and banana",
+    this method returns 5 (it finds BOTH "A" and "a")
+    - if character = "A" and searchString = "Apple and banana",
+    this method returns 5 (it finds BOTH "A" and "a")
+    - if character = "!" and searchString = "Hello! Nice day!",
+    this method returns 2
+
+
+    DO THIS WITH A FOR LOOP
+    */
+    public int countCharacters(String character, String searchString) {
+        /* to be implemented */
+        int counter = 0;
+        searchString = searchString.toLowerCase();
+        character = character.toLowerCase();
+        for (int i = 0; i < searchString.length(); i ++) {
+            if (searchString.substring(i, i +1).equals(character)) {
+                counter ++;
+            }
+        }
+        return counter;
+    }
+
+
+    /* Returns the original string reversed
+
+
+    Examples:
+    - if origString = "hello!" this method returns "!olleh"
+    - if origString = "Apples and bananas" this method returns "sananab dna selppA"
+    */
+    public String reverseString(String origString) {
+        /* to be implemented */
+        String tempStr = "";
+        for (int i = origString.length() - 1; i >= 0; i--) {
+            tempStr += origString.substring(i , i + 1);
+        }
+        return tempStr;
+    }
+
+    public String removeA(String str) {
+        String tempStr = "";
+        str = str.toLowerCase();
+        for (int i = 0; i < str.length(); i ++) {
+            if (str.substring(i, i +1).equals("a")) {
+
+            } else {
+                tempStr += str.substring(i, i +1);
+            }
+        }
+        return tempStr;
+    }
+    public String replaceCharacterV1(String searchChar, String origStr, String replaceChar){
+        /* to be implemented */
+        String newStr = "";
+        for (int i = 0; i < origStr.length(); i ++) {
+            if (origStr.substring(i, i + 1).equals(searchChar)) {
+                newStr += replaceChar;
+            } else {
+                newStr += origStr.substring(i, i + 1);
+            }
+        }
+        return newStr;
+    }
+    public String replaceCharacterV2(String searchChar, String origStr, String replaceChar){
+        /* to be implemented */
+        int i = 0;
+        while (origStr.indexOf(searchChar) != -1) {
+            i = origStr.indexOf(searchChar);
+            origStr = origStr.substring(0, i ) + replaceChar + origStr.substring(i +1 );
+        }
+        return origStr;
+    }
+    public int countString(String searchString, String origString) {
+        /* to be implemented */
+        int value = searchString.length();
+        int counter = 0;
+        for (int i = 0; i < origString.length() - value + 1 ; i ++) {
+            if (origString.substring(i, i + value ).equals(searchString)) {
+                counter ++;
+            }
+        }
+        return counter;
+    }
+    public String removeString(String searchString, String origString) {
+        /* to be implemented */
+        int i = 0;
+        int value = searchString.length();
+        while (origString.indexOf(searchString) != -1) {
+            i = origString.indexOf(searchString);
+            origString = origString.substring(0, i) + origString.substring(i + value);
+        }
+        return origString;
+    }
+    public void commaSeparated(int fromNum, int toNum) {
+        /* to be implemented */
+        if (fromNum >= toNum) {
+            for (int i = fromNum; i >= toNum; i--) {
+                System.out.print(i);
+                if (i > toNum) {
+                    System.out.print(", ");
+                }
+            }
+        } else {
+            for (int i = fromNum; i <= toNum; i++) {
+                System.out.print(i);
+                if (i < toNum) {
+                    System.out.print(", ");
+                }
+            }
+        }
+        System.out.println();
+    }
+    public boolean isPalindrome(String myString) {
+        /* to be implemented */
+        String newstr = removeString(" ", myString);
+        if (reverseString(newstr).equals(newstr)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
+
